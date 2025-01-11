474ae3a956bd868a24181c1b271c1f69f4005705
rami44 <53144875+rami44@users.noreply.github.com>
2025-01-11 16:30:06 +0200

    Update request.h

--- request.h
+++ request.h
@@ -1,1 +1,23 @@
+#ifndef __REQUEST_H__
+#define __REQUEST_H__
+
+#include "queue.h"
+#include <sys/time.h>
+#include <pthread.h>
+
+typedef struct threadStats {
+    pthread_t ourThread;
+    int id;
+    int stat_req;
+    int dynm_req;
+    int total_req;
+    
+} threadStats;
+
+// Called by your threads to handle a request
+void requestHandle(int fd, Node node, threadStats *t_stats);
+
+Node skip_request(threadStats* thread);
+
+#endif
 
